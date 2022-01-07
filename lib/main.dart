076dc8ce07ee57d7e204caf1207b6a183e22a63f
kirill <repetitorkazan@mail.ru>
2022-01-07 20:58:56 +0300

    ebychyi sluchay (learn flutter create app bar)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/location_detail/location_detail.dart';
+import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LocationDetail(),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+
+  final String title;
+
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10
+        ),
+        child: Text(title),
+      ),
+      iconTheme: const IconThemeData(color: Colors.black),
+      actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.favorite))],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+}
